b090d9c600ad38d3632bd855fb5d2a85753321b7
Hampus <hjm@ownit.nu>
2021-11-19 15:07:25 +0100

    Base app looks

--- app/src/main/java/com/example/moviejournal/MainActivity.kt
+++ app/src/main/java/com/example/moviejournal/MainActivity.kt
@@ -6,18 +6,23 @@ import android.widget.Button
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Handler
+import com.google.firebase.auth.FirebaseAuth
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        auth = FirebaseAuth.getInstance()
 
         Handler().postDelayed({
-            val i = Intent(this, films::class.java)
+//            val i = Intent(this, films::class.java)
+            val i = Intent(this, login::class.java)
             startActivity(i)
             finish()
-        }, 3000)
+        }, 2000)
     }
 }
