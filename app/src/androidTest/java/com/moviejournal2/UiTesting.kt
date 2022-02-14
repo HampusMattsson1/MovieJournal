7e4d5d486788422a4731b0d24748a0d5028345c3
Hampus <hjm@ownit.nu>
2022-02-14 10:29:28 +0100

    Progress on testing

--- app/src/androidTest/java/com/moviejournal2/UiTesting.kt
+++ app/src/androidTest/java/com/moviejournal2/UiTesting.kt
@@ -1,13 +1,16 @@
 package com.moviejournal2
 
+import androidx.core.content.MimeTypeFilter.matches
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.espresso.Espresso.onView
 import androidx.test.core.app.ActivityScenario
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.replaceText
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.EspressoException
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import kotlinx.coroutines.delay
 
@@ -16,6 +19,7 @@ import org.junit.runner.RunWith
 
 import org.junit.Assert.*
 import org.junit.Rule
+import java.util.regex.Pattern.matches
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -27,20 +31,94 @@ class UITests {
 
     @get:Rule var a : ActivityScenarioRule<sign_in> = ActivityScenarioRule(sign_in::class.java)
 
-
     // Login tests
     @Test
     fun loginWithoutCredentials() {
         onView(withId(R.id.btn_login)).perform(click())
+//        onView(withText("Please enter email").check(matches(isDisplayed())))
+//        onView(withId(R.id.btn_login))
+//            .perform(click())
+//            .check(matches(isDisplayed()))
     }
 
     @Test
-    fun loginTest1() {
+    fun loginWithoutEmail() {
+        onView(withId(R.id.login_textEditPassword)).perform(replaceText("hamt19"))
+        onView(withId(R.id.btn_login)).perform(click())
+    }
+
+    @Test
+    fun loginWithoutPassword() {
+        onView(withId(R.id.login_textEditEmail)).perform(replaceText("hjm@ownit.nu"))
+        onView(withId(R.id.btn_login)).perform(click())
+    }
+
+    @Test
+    fun loginSuccess() {
         onView(withId(R.id.login_textEditEmail)).perform(replaceText("hjm@ownit.nu"))
         onView(withId(R.id.login_textEditPassword)).perform(replaceText("hamt19"))
         onView(withId(R.id.btn_login)).perform(click())
     }
 
+    @Test
+    fun movieSearch() {
+        onView(withId(R.id.login_textEditEmail)).perform(replaceText("hjm@ownit.nu"))
+        onView(withId(R.id.login_textEditPassword)).perform(replaceText("hamt19"))
+        onView(withId(R.id.btn_login)).perform(click())
+        Thread.sleep(3000)
+
+        onView(withId(R.id.ic_search)).perform(click())
+        onView(withId(R.id.textEditSearch)).perform(replaceText("spider-man"))
+        onView(withId(R.id.btn_search)).perform(click())
+    }
+
+    @Test
+    fun searchUser() {
+        onView(withId(R.id.login_textEditEmail)).perform(replaceText("hjm@ownit.nu"))
+        onView(withId(R.id.login_textEditPassword)).perform(replaceText("hamt19"))
+        onView(withId(R.id.btn_login)).perform(click())
+        Thread.sleep(3000)
+
+        onView(withId(R.id.ic_people)).perform(click())
+        onView(withId(R.id.userSearch)).perform(replaceText("batman"))
+        onView(withId(R.id.searchButton)).perform(click())
+        Thread.sleep(2000)
+    }
+
+    @Test
+    fun editProfile() {
+        onView(withId(R.id.login_textEditEmail)).perform(replaceText("hjm@ownit.nu"))
+        onView(withId(R.id.login_textEditPassword)).perform(replaceText("hamt19"))
+        onView(withId(R.id.btn_login)).perform(click())
+        Thread.sleep(3000)
+
+        onView(withId(R.id.ic_profile)).perform(click())
+        onView(withId(R.id.settings)).perform(click())
+        onView(withId(R.id.username)).perform(replaceText("UITest"))
+        onView(withId(R.id.username)).perform(swipeUp())
+        Thread.sleep(3000)
+        onView(withId(R.id.save)).perform(click())
+        Thread.sleep(5000)
+    }
+
+    @Test
+    fun test() {
+//        onView(withId(R.id.bottom_navigation)).check(matches(isDisplayed()))
+
+//        onView(withText("Page Two Title")).check(isDisplayed())
+
+//        onView(withId(R.id.login_textEditEmail)).perform(replaceText("hjm@ownit.nu"))
+//        onView(withId(R.id.login_textEditPassword)).perform(replaceText("hamt19"))
+//        onView(withId(R.id.btn_login)).perform(click())
+//        Thread.sleep(2000)
+
+//        onView(withId(R.id.ic_journal)).perform(click())
+
+//        onView(withText("Your watchlist")).check(matches(isDisplayed()))
+
+
+
+    }
 
 
 }
--- app/src/test/java/com/moviejournal2/ValidatorTest.kt
+++ app/src/test/java/com/moviejournal2/ValidatorTest.kt
@@ -1,30 +1,53 @@
 package com.moviejournal2
 
 import MoviesAdapter
+import android.util.Log
+import org.assertj.core.api.Assertions
 import org.junit.Assert.*
 import org.junit.Before
+import org.junit.FixMethodOrder
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 
 
 @RunWith(JUnit4::class)
-class ValidatorTest {
+class UnitTestMoviesAdapter {
 
+    val movieList: MutableList<Movie> = ArrayList()
+    var ma: MoviesAdapter = MoviesAdapter(
+        movieList,
+        ::temp
+    )
+
+    // Movie object
+    var movie: Movie = Movie(
+        123,"TestTitle","TestOverview","TestPath",
+        "TestBackdrop", 0.5, "2021/05/30")
+
+    fun temp(m:Movie): Unit {
+        Log.d("test", "test")
+    }
 
     @Test
-    fun testMoviesAdapter() {
+    fun initMoviesAdapter() {
 
-        // Creating a movie object
-        var movie: Movie = Movie(
-            123,"TestTitle","TestOverview","TestPath",
-            "TestBackdrop", 0.5, "2021/05/30")
-
+        // Create array
         val movieList: MutableList<Movie> = ArrayList()
+//        movieList.add(0, movie)
 
-        val m = MoviesAdapter(movieList, movie)
+        Assertions.assertThat(ma.itemCount).isEqualTo(0)
+    }
 
-//        Assertions.assertThat(test).isNotNull
+    @Test
+    fun appendMovie() {
+
+        val list: MutableList<Movie> = ArrayList()
+        list.add(0, movie)
+
+        ma.appendMovies(list)
+
+        Assertions.assertThat(ma.itemCount).isEqualTo(1)
 
     }
 
